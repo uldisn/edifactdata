025974f3594c720cb52d4b40d9c9a22eb172ec39
Edmunds Priede <edmunds@kls.lv>
2015-01-02 11:01:11 +0200

    jaunas tabules

---
+++ migrations/m150102_105506_ccmc_create.php
@@ -0,0 +1,55 @@
+<?php
+
+class m141109_192506_ecnt_create extends EDbMigration
+{
+	public function up()
+	{
+         $this->execute("
+           CREATE TABLE `ccmc_move_codes` (
+  `ccmc_code` varchar(2) NOT NULL,
+  `ccmc_short` varchar(100) DEFAULT NULL,
+  `ccmc_long` varchar(255) DEFAULT NULL,
+  `ccmc_css_class` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ccmc_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+             
+        ");
+	
+        
+        
+       
+         $this->execute("
+           insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('DE','Discharge empty','Empty container discharged from vessel','row_dischempty');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('DF','Discharge full','Full container discharged from vessel','row_dischfull');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('LD','Local devaning','Container dispatched for discharge','row_localdevaning');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('LV','Local vaning','Empty container dispatched for export loading','row_localvaning');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('TE','Terminal empty','Emply container returned to terminal','row_terminalempty');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('TF','Terminal full','Full container returned to terminal','row_terminalfull');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('VE','Vessel empty','Empty container loaded to vessel','row_vesselempty');
+insert  into `ccmc_move_codes`(`ccmc_code`,`ccmc_short`,`ccmc_long`,`ccmc_css_class`) values ('VF','Vessel full','Full container loaded to vessel','row_vesselfull');
+
+
+             
+        ");
+	}
+
+	public function down()
+	{
+         $this->execute("
+            DROP TABLE `ccmc_move_codes`;
+             
+        ");
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
---
+++ migrations/m150102_105906_cctc_create.php
@@ -0,0 +1,53 @@
+<?php
+
+class m141109_192506_ecnt_create extends EDbMigration
+{
+	public function up()
+	{
+         $this->execute("
+           CREATE TABLE `cctc_type_codes` (
+  `cctc_code` varchar(4) NOT NULL,
+  `cctc_description` varchar(100) DEFAULT NULL,
+  `cctc_css_class` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cctc_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+             
+        ");
+	
+        
+        
+       
+         $this->execute("
+           insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('2200','20\' Dry Van','label label-large ');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('22G0','20\' Dry Van','label label-large ');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('22G1','20\' Dry Van','label label-large ');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('4200','40\' Dry Van','label label-large arrowed-right');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('4500','40\' High Cube','label label-large  arrowed-right label-info');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('45G0','40\' High Cube','label label-large  arrowed-right label-info');
+insert  into `cctc_type_codes`(`cctc_code`,`cctc_description`,`cctc_css_class`) values ('45G1','40\' High Cube','label label-large  arrowed-right label-info');
+
+             
+        ");
+	}
+
+	public function down()
+	{
+         $this->execute("
+            DROP TABLE `ccmc_move_codes`;
+             
+        ");
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
